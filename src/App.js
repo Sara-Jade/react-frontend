227bba95b12d10038fa55a5580427bdc6f8edb39
Super Juicer <you@example.com>
2022-08-19 15:54:25 -0700

    The frontend gets the data by showing alerts. It does not work by clicking the buttons as intended.

--- src/App.js
+++ src/App.js
@@ -1,24 +1,41 @@
 // import logo from './logo.svg';
+import { useEffect, useState } from 'react';
 import './App.css';
 
 function App() {
-  function callAPI(path) {
-    let request = new XMLHttpRequest();
-    request.open("GET", `https://localhost:44375/${path}`);
-    request.send();
-    request.onload = () => {
-      if (request.status === 200) {
-          alert(request.response);
-      } else {
-          alert(`Error: ${request.status} ${request.statusText}`);
+  const [data, setData] = useState(null);
+
+    function callAPI(path) {
+      {/*let request = new XMLHttpRequest();
+      request.open("GET", `https://localhost:44375/${path}`);
+      request.send();
+      request.onload = () => {
+        if (request.status === 200) {
+            alert(request.response);
+        } else {
+            alert(`Error: ${request.status} ${request.statusText}`);
+        }
       }
-    }
+    }*/}
+
+    {/*fetch(`https://localhost:44375/${path}`)
+      .then(response => response.json())
+      .then(setData)
+  .catch(error => (console.log(error)));*/}
+    
+    useEffect(() => {
+      fetch(`https://localhost:44375/${path}`)
+      .then(response => response.json())
+      .then(data => setData(data))
+      .catch(error => console.log(error));
+    }, []);
   }
 
   return (
     <div className="App">
-      <button onclick={callAPI('books')}>Get Books</button>
-      <button onclick={callAPI('authors')}>Get Authors</button>
+      <button onClick={callAPI('books')}>Get Books</button>
+      <button onClick={callAPI('authors')}>Get Authors</button>
+      {/*<p style={{wordBreak: 'break-all'}}>{JSON.stringify(data)}</p>*/}
     </div>
   );
 }
