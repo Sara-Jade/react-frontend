94f39b5712cac8ba9e93630db9189f83a2bfa0d0
Super Juicer <you@example.com>
2022-08-20 14:07:21 -0700

    App.js now displays either authors or books, depending on which line is commented in

--- src/App.js
+++ src/App.js
@@ -3,39 +3,22 @@ import { useEffect, useState } from 'react';
 import './App.css';
 
 function App() {
+
   const [data, setData] = useState(null);
-
-    function callAPI(path) {
-      {/*let request = new XMLHttpRequest();
-      request.open("GET", `https://localhost:44375/${path}`);
-      request.send();
-      request.onload = () => {
-        if (request.status === 200) {
-            alert(request.response);
-        } else {
-            alert(`Error: ${request.status} ${request.statusText}`);
-        }
-      }
-    }*/}
-
-    {/*fetch(`https://localhost:44375/${path}`)
-      .then(response => response.json())
-      .then(setData)
-  .catch(error => (console.log(error)));*/}
-    
-    useEffect(() => {
-      fetch(`https://localhost:44375/${path}`)
-      .then(response => response.json())
-      .then(data => setData(data))
-      .catch(error => console.log(error));
-    }, []);
-  }
+  // const path = 'authors';
+  const path = 'books';
+  useEffect(() => {
+    fetch(`https://localhost:44375/${path}`)
+    .then(response => response.json())
+    .then(data => setData(data))
+    .catch(error => console.log(error));
+  }, []);
 
   return (
     <div className="App">
-      <button onClick={callAPI('books')}>Get Books</button>
-      <button onClick={callAPI('authors')}>Get Authors</button>
-      {/*<p style={{wordBreak: 'break-all'}}>{JSON.stringify(data)}</p>*/}
+      {/*<button onClick={callAPI('books')}>Get Books</button>
+      <button onClick={callAPI('authors')}>Get Authors</button>*/}
+      <p style={{wordBreak: 'break-all'}}>{JSON.stringify(data)}</p>
     </div>
   );
 }
